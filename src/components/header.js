0b76e9913248b3fe78babf96fb83876d84bf2da2
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-07-30 17:17:31 -0700

    links css component to pages

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,3 @@ import React from "react"
 export default function Header(props) {
   return <h1>{props.headerText}</h1>
 }
-
-// import React from "react"
-//
-// export default function Header(props) {
-//   return <h1>{props.headerText}</h1>
-// }
